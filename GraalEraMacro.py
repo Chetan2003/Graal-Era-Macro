c99c55ed2478b1f247ef58fd1628323537eb1f53
Chetan Varma <72537908+Chetan2003@users.noreply.github.com>
2024-03-11 14:46:39 +0530

    The macro file

---
+++ GraalEraMacro.py
@@ -0,0 +1,26 @@
+import pyautogui
+import time
+import keyboard
+import sys
+
+def StartMining(x,y):
+    
+    timeMining = 45
+    start_time = time.time()
+    pyautogui.moveTo(x,y,duration=1)
+    while(time.time() - start_time) < timeMining:
+        if(keyboard.is_pressed('q')):
+            sys.exit()
+        pyautogui.click(x,y,duration = 1.2)
+
+
+time.sleep(2)
+def StartProgram():
+    while True:
+        if(keyboard.is_pressed('q')):
+            break
+        else:
+            StartMining(952,919)
+            
+            StartMining(890,852)
+StartProgram()
